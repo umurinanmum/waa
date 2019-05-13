eb0af743326d438e5500fb3475cf9292db30f37e
umur inan <umurinan@gmail.com>
2019-05-13 10:26:23 -0500

    Block Service

--- Server/src/main/java/edu/mum/waa/service/BlockServiceImpl.java
+++ Server/src/main/java/edu/mum/waa/service/BlockServiceImpl.java
@@ -1,8 +1,10 @@
 package edu.mum.waa.service;
 
 import edu.mum.waa.dto.BlockDto;
+import edu.mum.waa.entity.Block;
 import edu.mum.waa.repository.BlockRepo;
 import edu.mum.waa.service.interfaces.BlockService;
+import lombok.var;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,26 +22,39 @@ public class BlockServiceImpl implements BlockService {
 
     @Override
     public boolean save(BlockDto blockDto) {
-        return false;
+        Block block = blockDto.convertToEntity(blockDto);
+        blockRepo.save(block);
+        return true;
     }
 
     @Override
     public boolean delete(Long id) {
-        return false;
+        blockRepo.deleteById(id);
+        return true;
     }
 
     @Override
     public boolean update(BlockDto blockDto) {
-        return false;
+        Block block = blockDto.convertToEntity(blockDto);
+        blockRepo.save(block);
+        return true;
     }
 
     @Override
     public ArrayList<BlockDto> findAll() {
-        return null;
+        var res = blockRepo.findAll();
+        ArrayList<BlockDto> result = new ArrayList<>();
+        BlockDto temp = new BlockDto();
+        for (Block block : res) {
+            result.add(temp.convertToDto(block));
+        }
+        return result;
     }
 
     @Override
     public BlockDto findById(Long id) {
-        return null;
+        var res = blockRepo.findById(id);
+        BlockDto temp = new BlockDto();
+        return temp.convertToDto(res.get());
     }
 }
