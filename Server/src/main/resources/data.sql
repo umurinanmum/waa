e3f5d6afeaff02e7075720e93588be94ecf4d081
zmeid <zmeid.krtz@gmail.com>
2019-05-13 13:33:40 -0500

    SQL init queries added

---
+++ Server/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+/*INSERT INTO session (start_Week, end_Week, start_Day,end_Day) VALUES
+  ('1', '3', '1','3'),
+  ('3', '4', '1','3');*/
+
+  INSERT INTO block (id,start_Date, end_Date,canceled_days) VALUES
+  (1,'2017-08-04', '2017-08-04',0);
+
+
+/*  INSERT INTO entry (id,start_Date, end_Date,canceled_days) VALUES
+  (1,'2017-08-04', '2017-08-04',0);*/
