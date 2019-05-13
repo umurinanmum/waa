775659baa720f015f6206f400c6b405472fa75b2
umur inan <umurinan@gmail.com>
2019-05-13 12:01:10 -0500

    add block form example

--- Client/src/main/java/edu/mum/client/controller/BlockController.java
+++ Client/src/main/java/edu/mum/client/controller/BlockController.java
@@ -6,11 +6,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/block")
 public class BlockController {
 
-    @GetMapping("/block/show-add-form")
+    @GetMapping("/show-add-form")
     public String showAddForm(@ModelAttribute BlockModel blockModel, Model model){
 
         return "block-add";
--- Client/src/main/java/edu/mum/client/model/BlockModel.java
+++ Client/src/main/java/edu/mum/client/model/BlockModel.java
@@ -2,6 +2,7 @@ package edu.mum.client.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -9,8 +10,10 @@ import java.time.LocalDate;
 @Setter
 public class BlockModel {
 
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate startDate;
 
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate endDate;
 
     private int cancelledDays=0;
