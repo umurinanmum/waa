f962e782d29dc9920ef76c5459f608e1c235f6b6
zmeid <zmeid.krtz@gmail.com>
2019-05-14 10:43:53 -0500

    data.sql updated

--- Server/src/main/resources/data.sql
+++ Server/src/main/resources/data.sql
@@ -5,9 +5,19 @@
   INSERT INTO block (id,start_Date, end_Date,canceled_days,available_days) VALUES
   (1 ,'2017-08-04', '2017-08-04',0,22 );
 
-    INSERT INTO student (id,barcode, studentId,canceled_days,available_days) VALUES
-  (1 ,'2017-08-04', '2017-08-04',0,22 );
+    INSERT INTO student (id,barcode, student_Id) VALUES
+  (1 ,'12345343', '987012' );
 
+
+      INSERT INTO credential (id,email, password,student_id) VALUES
+  (1 ,'eozturk@mum.edu', '12345',1);
+
+        INSERT INTO role (id,name) VALUES
+  (1 ,'GENERATEREPORT');
+
+
+      INSERT INTO credential_roles (credential_id,roles_id) VALUES
+  (1 ,1);
 
 /*  INSERT INTO entry (id,start_Date, end_Date,canceled_days) VALUES
   (1,'2017-08-04', '2017-08-04',0);*/
