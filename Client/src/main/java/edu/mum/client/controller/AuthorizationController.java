bc7c076229a8e0cb24e43b4aee45091e5bf37c00
umur inan <umurinan@gmail.com>
2019-05-15 10:31:34 -0500

    security

--- Client/src/main/java/edu/mum/client/controller/AuthorizationController.java
+++ Client/src/main/java/edu/mum/client/controller/AuthorizationController.java
@@ -1,11 +1,15 @@
 package edu.mum.client.controller;
 
+import edu.mum.client.helper.Constants;
 import edu.mum.client.model.LoginModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 @RequestMapping("/authorization")
@@ -13,17 +17,22 @@ public class AuthorizationController {
 
 
     @GetMapping
-    public String showLoginForm(@ModelAttribute LoginModel loginModel){
+    public String showLoginForm(@ModelAttribute LoginModel loginModel) {
 
         return "login";
     }
 
     @PostMapping("/do-login")
-    public String doLogin(@ModelAttribute LoginModel loginModel){
+    public String doLogin(@ModelAttribute LoginModel loginModel) {
 
-
-
-        return "welcome";
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> result = restTemplate.postForEntity(Constants.URL_AUTH, loginModel, String.class);
+        System.out.println(result.getBody());
+        if (result.getBody() != null && !result.getBody().trim().isEmpty()) {
+            SecurityContextHolder.getContext().getAuthentication().setAuthenticated(true);
+            return "welcome";
+        }
+        return "redirect:/authorization";
     }
 
 }
---
+++ Client/src/main/java/edu/mum/client/helper/Constants.java
@@ -0,0 +1,8 @@
+package edu.mum.client.helper;
+
+public class Constants {
+
+
+    public static final String URL_AUTH ="http://localhost:8081/v1/authentication";
+    public static final String URL ="http://localhost:8081/api/v1/";
+}
