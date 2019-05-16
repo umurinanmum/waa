879fdc5f1521aab068f4e84571acfa15c5e392ba
zmeid <zmeid.krtz@gmail.com>
2019-05-15 22:01:49 -0500

    data.sql updated

--- Server/src/main/resources/data.sql
+++ Server/src/main/resources/data.sql
@@ -2,15 +2,32 @@
   ('1', '3', '1','3'),
   ('3', '4', '1','3');*/
 
-  INSERT INTO block (id,start_Date, end_Date,canceled_days,available_days) VALUES
+
+INSERT INTO student (id,barcode, student_Id) VALUES(1 ,'7888', '987012' );
+INSERT INTO student (id,barcode, student_Id) VALUES(2 ,'456', '986814' );
+
+
+INSERT INTO credential (id,email, password,student_id) VALUES (1 ,'eozturk@mum.edu', '12345',1);
+INSERT INTO credential (id,email, password,student_id) VALUES (2 ,'tinan@mum.edu', '12345',2);
+
+INSERT INTO role (id,name) VALUES (1 ,'GENERATE_REPORT');
+INSERT INTO role (id,name) VALUES (2 ,'SAVE_ENTRY');
+INSERT INTO role (id,name) VALUES (3 ,'SAVE_BLOCK');
+
+
+
+INSERT INTO credential_roles (credential_id,roles_id) VALUES  (1 ,1);
+
+INSERT INTO credential_roles (credential_id,roles_id) VALUES  (2 ,1);
+INSERT INTO credential_roles (credential_id,roles_id) VALUES  (2 ,2);
+INSERT INTO credential_roles (credential_id,roles_id) VALUES  (2 ,3);
+
+
+
+
+INSERT INTO block (id,start_Date, end_Date,canceled_days,available_days) VALUES
   (1 ,'2016-11-01', '2016-11-30',0,22 );
 
-    INSERT INTO student (id,barcode, student_Id) VALUES
-  (1 ,'7888', '987012' );
-
-/*      INSERT INTO student (id,barcode, student_Id) VALUES
-  (2 ,'7326', '987013' );
-*/
 
 
     INSERT INTO location (id,location_name,location_code) VALUES
@@ -20,15 +37,6 @@
   (2 ,'Art Center','AC' );
 
 
-      INSERT INTO credential (id,email, password,student_id) VALUES
-  (1 ,'eozturk@mum.edu', '12345',1);
-
-        INSERT INTO role (id,name) VALUES
-  (1 ,'GENERATEREPORT');
-
-
-      INSERT INTO credential_roles (credential_id,roles_id) VALUES
-  (1 ,1);
 
 /*  INSERT INTO entry (id,start_Date, end_Date,canceled_days) VALUES
   (1,'2017-08-04', '2017-08-04',0);*/
