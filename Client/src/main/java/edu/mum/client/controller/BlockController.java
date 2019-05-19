dee02b7eaeddc1415f58ab74097b78b56f92cffb
gTuugii <tuugii83@gmail.com>
2019-05-18 21:40:32 -0500

    add Block - list, add, model,

--- Client/src/main/java/edu/mum/client/controller/BlockController.java
+++ Client/src/main/java/edu/mum/client/controller/BlockController.java
@@ -1,8 +1,9 @@
 package edu.mum.client.controller;
 
 
+import edu.mum.client.helper.Constants;
 import edu.mum.client.helper.TokenHelper;
-import edu.mum.client.model.BlockReportModel;
+import edu.mum.client.model.BlockModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -11,22 +12,24 @@ import org.springframework.http.ResponseEntity;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Controller
-@RequestMapping("/block")
+@RequestMapping("/blocks")
 public class BlockController {
 
 
-    private String api_url = "http://localhost:8081/api/v1/blocks"; //Constants.URL + "students";
+    private String api_url = Constants.URL + "blocks";
 
     private final TokenHelper tokenHelper;
 
@@ -35,36 +38,157 @@ public class BlockController {
         this.tokenHelper = tokenHelper;
     }
 
-    @GetMapping
+    @GetMapping("/list")
     public String getAll(Model model){
 
-        RestTemplate restTemplate = new RestTemplate();
+        try{
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", "Bearer " + tokenHelper.getToken());
+            HttpEntity<BlockModel[]> entity = new HttpEntity<BlockModel[]>(headers);
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<BlockModel[]> response = restTemplate.exchange(api_url, HttpMethod.GET, entity, BlockModel[].class);
+            final List<BlockModel> students = Arrays.stream(response.getBody()).collect(Collectors.toList());
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + tokenHelper.getToken());
-
-        HttpEntity entity = new HttpEntity(headers);
-        ResponseEntity<List> exchange =restTemplate.exchange(api_url, HttpMethod.GET, entity, List.class);
-
-        //ResponseEntity<String> response = restTemplate.exchange(api_url, HttpMethod.GET, entity, String.class);
-
-        System.out.println("response: " + exchange.getBody());
-
-        model.addAttribute("blocks", exchange.getBody());
+            model.addAttribute("blocks", students);
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
 
         return "blocks/block-list";
     }
 
-
-    @GetMapping("/show-add-form")
-    public String showAddForm(@ModelAttribute BlockReportModel blockReportModel, Model model){
-        return "block-add";
+    @GetMapping("/add")
+    public String add(@ModelAttribute BlockModel blockModel){
+        System.out.println("blocks/block-add");
+        return "blocks/block-add";
     }
 
-    @PostMapping("/save-block")
-    public String saveBlock(@ModelAttribute BlockReportModel blockReportModel){
+    @PostMapping("/add")
+    public String save(@Valid @ModelAttribute BlockModel blockModel,
+                       BindingResult bindingResult,
+                       RedirectAttributes redirectAttributes,
+                       Model model){
+        String result_str = "saved";
+        try {
+            if (bindingResult.hasErrors()) {
+                return "blocks/block-add";
+            }
 
-        return "welcome";
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", "Bearer " + tokenHelper.getToken());
+            HttpEntity<BlockModel> entity = new HttpEntity<>(blockModel, headers);
+
+            RestTemplate restTemplate = new RestTemplate();
+
+            ResponseEntity<String> result = restTemplate.postForEntity(api_url, entity, String.class);
+            System.out.println("result: " + result.getBody());
+            if (result.getBody() == null || result.getBody().trim().isEmpty()) {
+                return "blocks/block-add";
+            }
+
+            if(!result.getBody().equalsIgnoreCase("true"))
+                result_str = result.getBody();
+
+            redirectAttributes.addFlashAttribute("blockModel", blockModel);
+            redirectAttributes.addFlashAttribute("result", result_str);
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+            model.addAttribute("result", e);
+            return "blocks/block-add";
+        }
+        return "redirect:list";
     }
+
+
+    @GetMapping("/edit/{blockid}")
+    public String edit(@PathVariable("blockid") Long blockid,
+                       Model model){
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", "Bearer " + tokenHelper.getToken());
+
+            HttpEntity entity = new HttpEntity<>(headers);
+            RestTemplate restTemplate = new RestTemplate();
+
+            ResponseEntity<BlockModel> response = restTemplate.exchange(api_url + "/" + blockid.toString(), HttpMethod.GET, entity, BlockModel.class);
+
+            BlockModel mod = response.getBody();
+            System.out.println("response: " + mod);
+
+            model.addAttribute("blockModel", mod);
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+
+        }
+        return "blocks/block-edit";
+    }
+
+    @PostMapping("/edit")
+    public String edit(@Valid @ModelAttribute BlockModel blockModel,
+                       BindingResult bindingResult,
+                       RedirectAttributes redirectAttributes,
+                       Model model){
+        try {
+            if (bindingResult.hasErrors()) {
+                return "blocks/block-edit";
+            }
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", "Bearer " + tokenHelper.getToken());
+            HttpEntity<BlockModel> entity = new HttpEntity<>(blockModel, headers);
+
+            RestTemplate restTemplate = new RestTemplate();
+
+            ResponseEntity<String> result = restTemplate.exchange(api_url, HttpMethod.PUT, entity, String.class);
+
+            System.out.println("result: " + result.getBody());
+            if (result.getBody() == null || result.getBody().trim().isEmpty()) {
+                return "blocks/block-edit";
+            }
+
+            String result_str = "edited";
+            if(!result.getBody().equalsIgnoreCase("true"))
+                result_str = result.getBody();
+
+            redirectAttributes.addFlashAttribute("result", result_str);
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+            model.addAttribute("result", e);
+            return "blocks/block-edit";
+        }
+        return "redirect:list";
+    }
+
+    @GetMapping("/delete/{blockid}")
+    public String delete(@PathVariable("blockid") Long blockid,
+                         RedirectAttributes redirectAttributes,
+                         Model model){
+
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", "Bearer " + tokenHelper.getToken());
+            HttpEntity entity = new HttpEntity<>(headers);
+
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<String> result = restTemplate.exchange(api_url + "/" + blockid, HttpMethod.DELETE, entity, String.class);
+            System.out.println("result: " + result.getBody());
+
+            String result_str = "Deleted";
+            if(!result.getBody().equalsIgnoreCase("true"))
+                result_str = result.getBody();
+
+            model.addAttribute("resultInfo", result_str);
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+            model.addAttribute("resultInfo", e);
+        }
+        return "result";
+    }
+
 
 }
---
+++ Client/src/main/java/edu/mum/client/model/BlockModel.java
@@ -0,0 +1,30 @@
+package edu.mum.client.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Getter @Setter
+public class BlockModel {
+
+    private Integer id;
+
+    @DateTimeFormat(pattern = "MM/dd/yyyy")
+    private LocalDate startDate;
+
+    @DateTimeFormat(pattern = "MM/dd/yyyy")
+    private LocalDate endDate;
+
+    @NotNull
+    private int cancelledDays;
+
+    @NotEmpty
+    @Size(min = 2, max = 50, message = "${Size.lastName}")
+    private String name;
+
+}
