e63edcb6f2a180b151ed1e9db1ebd2bd3fae5f38
zmeid <zmeid.krtz@gmail.com>
2019-05-21 14:49:41 -0500

    -Turkish language added, download report added

--- Client/src/main/java/edu/mum/client/controller/DownloadController.java
+++ Client/src/main/java/edu/mum/client/controller/DownloadController.java
@@ -4,6 +4,7 @@ import edu.mum.client.helper.Constants;
 import edu.mum.client.helper.TokenHelper;
 import edu.mum.client.model.BlockModel;
 import edu.mum.client.model.StudentReportModel;
+import edu.mum.client.model.StudentReportModelForFaculty;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
@@ -65,49 +66,42 @@ public class DownloadController {
         }
         return result;
 
-/*
-        try {
-            File file = new File("report.csv");
-            FileWriter fileWriter = new FileWriter(file);
-            fileWriter.write(fileContent);
-            fileWriter.flush();
-            fileWriter.close();
 
-            Path path = Paths.get("./report.csv");
-            Files.write(path, bytes);
+    }
 
 
+    @PostMapping(value = "/studentBlockReportToExcelByStudent" ,produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public @ResponseBody  byte[] studentBlockReportToExcelByStudent() {
+        StudentReportModelForFaculty studentReportModelSes=null;
+        Object studentReportModelSesObj =  httpSession.getAttribute("studentReportModelForFacultySes");
 
-            byte[] bytes = file.getBytes();
+        if(studentReportModelSesObj!=null){
+            studentReportModelSes = (StudentReportModelForFaculty) studentReportModelSesObj;
 
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
-
-
-
-        //If user is not authorized - he should be thrown out from here itself
-
-        //Authorized user will download the file
-/*        String dataDirectory = request.getServletContext().getRealPath("/WEB-INF/downloads/pdf/");
-        Path file = Paths.get(dataDirectory, fileName);
-        if (Files.exists(file))
-        {
-            response.setContentType("application/pdf");
-            response.addHeader("Content-Disposition", "attachment; filename="+fileName);
-            try
-            {
-                Files.copy(file, response.getOutputStream());
-                response.getOutputStream().flush();
-            }
-            catch (IOException ex) {
-                ex.printStackTrace();
-            }
         }
 
-        int a = 45;*/
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + tokenHelper.getToken());
+
+        HttpEntity<ResponseEntity<InputStreamResource>> entity = new HttpEntity<>(headers);
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<String> response = restTemplate.exchange(Constants.URL + "download/studentBlockReportToExcelForFaculty?blockName="+studentReportModelSes.getSelectedBlock()
+                        +"&id="+studentReportModelSes.getSelectedStudent()
+                , HttpMethod.GET, entity, String.class);
+
+        String fileContent="Empty";
+        fileContent = response.getBody();
+
+
+        InputStream inputStream = new ByteArrayInputStream(fileContent.getBytes(Charset.forName("UTF-8")));
+        byte[] result = null;
+        try {
+            result=IOUtils.toByteArray(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
 
 
     }
